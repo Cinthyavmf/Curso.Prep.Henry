df41cc0397926e550ddaf2a01f9569d2f5495b18
Cin Molina <cinthyavmf@gmail.com>
2022-03-02 16:48:37 -0300

    homework in progress

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -50,7 +50,16 @@ function deleteAbc(cadena){
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
-}
+  var nuevastring = '';
+  for (i = 0; i < cadena.length; i++) {
+    if (cadena[i] === "a" || cadena[i === "b" || cadena[i] === "c"]) {
+      continue;
+    }
+    else {
+      nuevastring = nuevastring + cadena[i];
+    }
+    return nuevastring;
+  }
 
 
 function sortArray(arr) {
